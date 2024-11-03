3cf3c01bbf7890da5ecb88528b36a8079b99ec78
Szabó Dávid <david030719@gmail.com>
2024-11-03 12:52:03 +0100

    Functionok külön fájlba rakása, handleSaveChanges refactor

---
+++ frontend/src/components/utils/functions.tsx
@@ -0,0 +1,121 @@
+import axios from "axios";
+import { CalendarEvent } from "../../types";
+
+export const fetchEvents = async (
+  setEvents: React.Dispatch<React.SetStateAction<CalendarEvent[]>>,
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>
+) => {
+  try {
+    const response = await axios.get(
+      "http://localhost:8080/user/calendar-events",
+      {
+        withCredentials: true,
+      }
+    );
+    setEvents(response.data);
+  } catch (err) {
+    console.error("Error fetching events: ", err);
+  } finally {
+    setLoading(false);
+  }
+};
+
+export const handleDeleteEvent = async (
+  eventId: string | undefined,
+  setEvents: React.Dispatch<React.SetStateAction<CalendarEvent[]>>,
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>,
+  closeDialog: () => void
+) => {
+  try {
+    await axios.delete(
+      `http://localhost:8080/user/calendar-events/${eventId}`,
+      {
+        withCredentials: true,
+        timeout: 5000,
+      }
+    );
+    closeDialog();
+    fetchEvents(setEvents, setLoading);
+  } catch (error) {
+    console.error("Error deleting event: ", error);
+  }
+};
+
+export const handleSaveChanges = async (
+  currentEvent: CalendarEvent | undefined,
+  eventStartDatePicker: Date | undefined,
+  eventStartTimeValue: string,
+  eventEndDatePicker: Date | undefined,
+  eventEndTimeValue: string,
+  setEvents: React.Dispatch<React.SetStateAction<CalendarEvent[]>>,
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>
+) => {
+  const esemenyNeve = document.getElementById(
+    "esemenyNeve"
+  ) as HTMLInputElement;
+  if (esemenyNeve) {
+    console.log("Az esemény új neve: " + esemenyNeve.value);
+  } else {
+    console.error("Az esemenyNeve nem található.");
+  }
+
+  if (currentEvent?.id) {
+    const startDateTime = new Date(currentEvent.start);
+    const endDateTime = new Date(currentEvent.end);
+    const combinatedStart = getCombinatedDateTime(
+      eventStartDatePicker,
+      eventStartTimeValue,
+      startDateTime
+    );
+    const combinatedEnd = getCombinatedDateTime(
+      eventEndDatePicker,
+      eventEndTimeValue,
+      endDateTime
+    );
+
+    try {
+      const updatedEvent: CalendarEvent = {
+        id: currentEvent.id,
+        summary: esemenyNeve.value || currentEvent.summary,
+        location: currentEvent.location || undefined,
+        description: currentEvent.description || undefined,
+        start: {
+          dateTime: combinatedStart || startDateTime.toISOString(),
+        },
+        end: {
+          dateTime: combinatedEnd || endDateTime.toISOString(),
+        },
+      };
+
+      console.log(updatedEvent);
+      const response = await axios.put(
+        `http://localhost:8080/user/calendar-events/${currentEvent.id}`,
+        updatedEvent,
+        {
+          withCredentials: true,
+          timeout: 5000,
+        }
+      );
+      console.log("Sikeres eseményfrissítés!", response.data);
+      fetchEvents(setEvents, setLoading);
+    } catch (error) {
+      console.error("Hiba történt az esemény frissítése során: ", error);
+    }
+  }
+};
+
+export const getCombinatedDateTime = (
+  datePicker: Date | undefined,
+  timeValue: string,
+  defaultDate: Date
+) => {
+  if (datePicker) {
+    const date = new Date(datePicker);
+    const [selectedHourString, selectedMinuteString] = timeValue.split(":");
+    const selectedHour: number = parseInt(selectedHourString, 10);
+    const selectedMinute: number = parseInt(selectedMinuteString, 10);
+    date.setHours(selectedHour, selectedMinute);
+    return date.toISOString();
+  }
+  return defaultDate.toISOString();
+};
--- frontend/src/controller/Calendar.tsx
+++ frontend/src/controller/Calendar.tsx
@@ -1,4 +1,3 @@
-import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { CalendarEvent } from "../types";
 import FullCalendar from "@fullcalendar/react";
@@ -11,7 +10,7 @@ import {
   DialogHeader,
   DialogTitle,
   DialogDescription,
-  DialogFooter
+  DialogFooter,
 } from "@/components/ui/dialog";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
@@ -24,11 +23,16 @@ import { Calendar as SmallCalendar } from "@/components/ui/smallcalendar";
 import {
   Popover,
   PopoverContent,
-  PopoverTrigger
+  PopoverTrigger,
 } from "@/components/ui/popover";
 import { format } from "date-fns";
+import {
+  fetchEvents,
+  handleDeleteEvent,
+  handleSaveChanges,
+} from "@/components/utils/functions";
 
-const Calendarr: React.FC = () => {
+const Calendar: React.FC = () => {
   const [events, setEvents] = useState<CalendarEvent[]>([]);
   const [loading, setLoading] = useState(true);
   const [isDialogOpen, setDialogOpen] = useState(false);
@@ -42,131 +46,44 @@ const Calendarr: React.FC = () => {
   const [eventEndDatePicker, setEventEndDatePicker] = React.useState<Date>();
   const [eventStartTimeValue, setEventStartTimeValue] =
     useState<string>("10:00");
-  const [eventEndTimeValue, setEventEndTimeValue] = useState<string>("00:00");
-  //Lekérdezi az eseményeket
-  const fetchEvents = async () => {
-    try {
-      const response = await axios.get(
-        "http://localhost:8080/user/calendar-events",
-        {
-          withCredentials: true
-        }
-      );
-      setEvents(response.data);
-    } catch (err) {
-      console.error("Error fetching events: ", err);
-    } finally {
-      setLoading(false);
-    }
-  };
+  const [eventEndTimeValue, setEventEndTimeValue] = useState<string>("12:00");
+
   //5 percenként újra lekérdezi az eseményeket.
   useEffect(() => {
-    fetchEvents();
+    fetchEvents(setEvents, setLoading);
 
     const interval = setInterval(fetchEvents, 300000); // 5 perc
     return () => clearInterval(interval);
   }, []);
 
-  //Az esemény törlése
-  const handleDeleteEvent = async (eventId: string | undefined) => {
-    try {
-      await axios.delete(
-        `http://localhost:8080/user/calendar-events/${eventId}`,
-        {
-          withCredentials: true,
-          timeout: 5000 // 5 másodperces időtúllépés teszteléshez
-        }
-      );
-      closeDialog();
-      fetchEvents();
-    } catch (error) {
-      console.error("Error deleting event: ", error);
-    }
+  const onDeleteEvent = (eventId: string | undefined) => {
+    handleDeleteEvent(eventId, setEvents, setLoading, closeDialog);
   };
-  //Szerkesztésnél a változtatások mentése
-  const handleSaveChanges = async () => {
-    const esemenyNeve = document.getElementById(
-      "esemenyNeve"
-    ) as HTMLInputElement;
-    if (esemenyNeve) {
-      console.log("Az esemény új neve: " + esemenyNeve.value);
-    } else {
-      console.error("Az esemenyNeve nem található.");
-    }
-    let combinatedStart;
-    let combinatedEnd;
-    if (currentEvent?.id) {
-      const startDateTime = new Date(currentEvent.start);
-      const endDateTime = new Date(currentEvent.end);
-      if(eventStartDatePicker != undefined){
-        //A dátum és idő kombinációja
-        //Először a START
-        const startDatum = new Date(eventStartDatePicker);
-        const [selectedStartHourString, selectedStartMinuteString] =
-          eventStartTimeValue.split(":");
-        const selectedStartHour: number = parseInt(selectedStartHourString, 10);
-        const selectedStartMinute: number = parseInt(
-          selectedStartMinuteString,
-          10
-        );
-        startDatum.setHours(selectedStartHour, selectedStartMinute);
-        combinatedStart = startDatum.toISOString();
-      }
 
-      if(eventEndDatePicker != undefined){
-        //És most az END
-        const endDatum = new Date(eventEndDatePicker);
-        const [selectedEndHourString, selectedEndMinuteString] =
-          eventEndTimeValue.split(":");
-        const selectedEndHour: number = parseInt(selectedEndHourString, 10);
-        const selectedEndMinute: number = parseInt(selectedEndMinuteString, 10);
-        endDatum.setHours(selectedEndHour, selectedEndMinute);
-        combinatedEnd = endDatum.toISOString();
-      }
-
-      try {
-        const updatedEvent: CalendarEvent = {
-          id: currentEvent.id,
-          summary: esemenyNeve.value || currentEvent.summary,
-          location: currentEvent.location || undefined,
-          description: currentEvent.description || undefined,
-          start: {
-            dateTime: combinatedStart || startDateTime.toISOString() // ISO 8601 formátum
-          },
-          end: {
-            dateTime: combinatedEnd || endDateTime.toISOString() 
-          }
-        };
-
-        console.log(updatedEvent);
-        const response = await axios.put(
-          `http://localhost:8080/user/calendar-events/${currentEvent.id}`,
-          updatedEvent,
-          {
-            withCredentials: true,
-            timeout: 5000
-          }
-        );
-        console.log("Sikeres eseményfrissítés!", response.data);
-        fetchEvents();
-      } catch (error) {
-        console.error("Hiba történt az esemény frissítése során: ", error);
-      }
-    }
+  const onSaveChanges = async () => {
+    await handleSaveChanges(
+      currentEvent as CalendarEvent,
+      eventStartDatePicker,
+      eventStartTimeValue,
+      eventEndDatePicker,
+      eventEndTimeValue,
+      setEvents,
+      setLoading
+    );
   };
-  //Dialog nyitása
+
   const openDialog = (eventInfo: any) => {
     setCurrentEvent({
       id: eventInfo.event.id,
       summary: eventInfo.event.title,
       start: eventInfo.event.start,
       end: eventInfo.event.end,
-      description: eventInfo.event.extendedProps.description
+      description: eventInfo.event.extendedProps.description,
     });
     setDialogOpen(true);
-    console.log("Dialog opening..."); // Teszt
+    console.log("Dialog opening...");
   };
-  //Dialog zárása
+
   const closeDialog = () => {
     setDialogOpen(false);
     setCurrentEvent(null);
@@ -175,29 +92,31 @@ const Calendarr: React.FC = () => {
   if (loading) {
     return <div>Betöltés...</div>;
   }
-
+  
   const eventList = events.map((event) => ({
     id: event.id,
     title: event.summary,
     start: event.start.dateTime,
     end: event.end.dateTime,
     location: event.location,
-    description: event.description
+    description: event.description,
   }));
+
   //Beleteszi a naptárba
   const renderEventContent = (eventInfo: any) => {
     const startTime = eventInfo.event.start
       ? new Date(eventInfo.event.start).toLocaleTimeString([], {
           hour: "2-digit",
-          minute: "2-digit"
+          minute: "2-digit",
         })
       : "";
     const endTime = eventInfo.event.end
       ? new Date(eventInfo.event.end).toLocaleTimeString([], {
           hour: "2-digit",
-          minute: "2-digit"
+          minute: "2-digit",
         })
       : "";
+
     return (
       <div onClick={() => openDialog(eventInfo)}>
         <b>
@@ -224,7 +143,7 @@ const Calendarr: React.FC = () => {
         headerToolbar={{
           left: "prev,next today",
           center: "title",
-          right: "dayGridMonth,timeGridWeek,timeGridDay"
+          right: "dayGridMonth,timeGridWeek,timeGridDay",
         }}
       />
       <Dialog open={isDialogOpen} onOpenChange={closeDialog}>
@@ -325,10 +244,10 @@ const Calendarr: React.FC = () => {
             </div>
           </div>
           <DialogFooter>
-            <Button onClick={() => handleDeleteEvent(currentEvent?.id)}>
+            <Button onClick={() => onDeleteEvent(currentEvent?.id)}>
               Törlés
             </Button>
-            <Button type="submit" onClick={handleSaveChanges}>
+            <Button type="submit" onClick={onSaveChanges}>
               Save changes
             </Button>
           </DialogFooter>
@@ -338,4 +257,4 @@ const Calendarr: React.FC = () => {
   );
 };
 
-export default Calendarr;
+export default Calendar;
