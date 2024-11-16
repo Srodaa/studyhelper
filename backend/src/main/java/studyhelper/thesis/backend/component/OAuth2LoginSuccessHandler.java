d1d9bb0d01e996d5744abc594ddd05915348cc23
Szabó Dávid <david030719@gmail.com>
2024-11-16 16:04:47 +0100

    refactor: OAuth2LoginSuccessHandler delete unnecessary imports, improve readability

--- backend/src/main/java/studyhelper/thesis/backend/component/OAuth2LoginSuccessHandler.java
+++ backend/src/main/java/studyhelper/thesis/backend/component/OAuth2LoginSuccessHandler.java
@@ -1,23 +1,18 @@
 package studyhelper.thesis.backend.component;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
-import studyhelper.thesis.backend.entity.CalendarEvent;
-import studyhelper.thesis.backend.service.CalendarService;
 import studyhelper.thesis.backend.service.UserService;
 
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 
 import java.io.IOException;
-import java.util.List;
 
 @Component
 public class OAuth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
@@ -25,40 +20,35 @@ public class OAuth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHan
     @Autowired
     private UserService userService;
 
-    @Autowired
-    private CalendarService googleCalendarService;  // Új service a Calendar API-hoz
+    private static final String REDIRECT_URL = "http://localhost:5173/home";
 
     @Autowired
-    private OAuth2AuthorizedClientService authorizedClientService; // Injektálás
+    private OAuth2AuthorizedClientService authorizedClientService;
 
 
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-        if (authentication == null || !(authentication instanceof OAuth2AuthenticationToken)) {
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+        if (authentication instanceof OAuth2AuthenticationToken token) {
+            handleOAuth2Authentication(token, request, response);
+        } else {
             response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Authentication required");
-            return;
         }
-        OAuth2AuthenticationToken token = (OAuth2AuthenticationToken) authentication;
-        OAuth2User oAuth2User = token.getPrincipal();
+    }
+
+    private void handleOAuth2Authentication(OAuth2AuthenticationToken token,HttpServletRequest request, HttpServletResponse response) throws IOException {
         String email = token.getPrincipal().getAttribute("email");
         String name = token.getPrincipal().getAttribute("name");
         String googleID = token.getPrincipal().getAttribute("sub");
 
-        // OAuth2AuthorizedClient betöltése
         OAuth2AuthorizedClient authorizedClient = authorizedClientService.loadAuthorizedClient(
                 token.getAuthorizedClientRegistrationId(), token.getName()
         );
 
-        // Access token lekérése
         String accessToken = authorizedClient.getAccessToken().getTokenValue();
-
-        // Itt felhasználhatod az access token-t, pl. mentheted a felhasználóhoz
-        // userService.saveUserIfNotExists(email, name, accessToken, googleID);
 
         userService.saveUserIfNotExists(email, name, null, googleID, accessToken);
 
-        String redirectURL = "http://localhost:5173/home";
-        getRedirectStrategy().sendRedirect(request, response, redirectURL);
-        super.onAuthenticationSuccess(request, response, authentication);
+        getRedirectStrategy().sendRedirect(request, response, REDIRECT_URL);
     }
+
 }
