9223b889b9ccefc995572fd61ecf9c9bac49cc9c
Szabó Dávid <david030719@gmail.com>
2025-01-19 16:23:21 +0100

    feat: update timer UI logic and styles

--- frontend/src/components/Timer.tsx
+++ frontend/src/components/Timer.tsx
@@ -16,6 +16,7 @@ const Timer: React.FC = () => {
   const [isRunning, setIsRunning] = useState<boolean>(false);
   const [intervalId, setIntervalId] = useState<NodeJS.Timeout | null>(null);
   const [isPopoverOpen, setIsPopoverOpen] = useState<boolean>(false);
+  const [isTimeVisible, setIsTimeVisible] = useState(false);
 
   const fetchCategories = async () => {
     try {
@@ -82,77 +83,96 @@ const Timer: React.FC = () => {
     return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
   };
 
-  const startStudy = isRunning ? formatTime(remainingTime) : "Start learning";
+  const startStudy = isRunning ? "Stop learning" : "Start learning";
+  const timerRemaining = isRunning ? formatTime(remainingTime) : "";
+  const mobileStartStudy = isRunning ? formatTime(remainingTime) : "Start learning";
 
   return (
-    <Popover open={isPopoverOpen} onOpenChange={handlePopoverOpenChange}>
-      <PopoverTrigger asChild>
-        <Button variant="outline" className="bg-slate-900 border border-slate-600 hover:bg-slate-800 hover:text-white">
-          {startStudy}
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-80 bg-slate-900 text-white border border-slate-600 border border-slate-600">
-        <div className="grid gap-4">
-          <div className="space-y-2">
-            <h4 className="font-medium leading-none text-center">Timer</h4>
-            <p className="text-sm text-slate-200 text-center">To start learning, select the points below.</p>
-          </div>
-          <div className="grid gap-2">
-            <div className="grid gap-4 grid-cols-3 items-center">
-              <Label htmlFor="category">Category</Label>
-              <Select value={selectedCategory} onValueChange={setSelectedCategory} onOpenChange={fetchCategories}>
-                <SelectTrigger className="w-[185px] border-slate-600 hover:border-white">
-                  <SelectValue placeholder="Choose your category" />
-                  <SelectContent>
-                    {categories.map((category, index) => (
-                      <SelectItem key={index} value={category}>
-                        {category}
-                      </SelectItem>
-                    ))}
-                  </SelectContent>
-                </SelectTrigger>
-              </Select>
-            </div>
-            <div className="grid grid-cols-3 items-center gap-4">
-              <Label htmlFor="duration">Duration (minutes)</Label>
-              <Input
-                type="number"
-                max="10000"
-                min="1"
-                id="duration"
-                value={remainingTime / 60}
-                onChange={(e) => {
-                  const newValue = e.target.value;
-                  if (newValue == "") {
-                    setDuration(0);
-                  } else {
-                    const parsedValue = parseInt(newValue, 10);
-                    setDuration(parsedValue);
-                  }
-                }}
-                className="col-span-2 h-8 border-slate-600 focus:border-white"
-              />
-            </div>
-          </div>
-          <div>
-            <Button
-              type="submit"
-              onClick={() => {
-                if (!selectedCategory || duration <= 0) {
-                  toast.error("Please select a category and set a valid duration.");
-                  return;
-                }
-                startTimer();
-                closePopover();
-              }}
-              className="grid float-right bg-white text-black hover:bg-slate-200 border border-slate-600"
-            >
-              Start
-            </Button>
-          </div>
+    <div>
+      {isTimeVisible && isRunning && (
+        <div className="flex flex-col items-center justify-center h-screen mobile:hidden">
+          <p className="font-mono text-9xl md:text-5xl">{timerRemaining}</p>
+          <p className="font-mono text-[10px] hover:block cursor-pointer" onClick={() => setIsTimeVisible(false)}>
+            hide
+          </p>
         </div>
-      </PopoverContent>
-    </Popover>
+      )}
+      <div className="flex items-center justify-center">
+        <Popover open={isPopoverOpen} onOpenChange={handlePopoverOpenChange}>
+          <PopoverTrigger asChild>
+            <Button
+              variant="outline"
+              className="bg-slate-900 border border-slate-600 hover:bg-slate-800 hover:text-white w-[120px]"
+            >
+              <span className="hidden mobile:block">{mobileStartStudy}</span>
+              <span className="block mobile:hidden">{startStudy}</span>
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-80 bg-slate-900 text-white border border-slate-600 border border-slate-600">
+            <div className="grid gap-4">
+              <div className="space-y-2">
+                <h4 className="font-medium leading-none text-center">Timer</h4>
+                <p className="text-sm text-slate-200 text-center">To start learning, select the points below.</p>
+              </div>
+              <div className="grid gap-2">
+                <div className="grid gap-4 grid-cols-3 items-center">
+                  <Label htmlFor="category">Category</Label>
+                  <Select value={selectedCategory} onValueChange={setSelectedCategory} onOpenChange={fetchCategories}>
+                    <SelectTrigger className="w-[185px] border-slate-600 hover:border-white">
+                      <SelectValue placeholder="Choose your category" />
+                      <SelectContent>
+                        {categories.map((category, index) => (
+                          <SelectItem key={index} value={category}>
+                            {category}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </SelectTrigger>
+                  </Select>
+                </div>
+                <div className="grid grid-cols-3 items-center gap-4">
+                  <Label htmlFor="duration">Duration (minutes)</Label>
+                  <Input
+                    type="number"
+                    max="10000"
+                    min="1"
+                    id="duration"
+                    value={remainingTime / 60}
+                    onChange={(e) => {
+                      const newValue = e.target.value;
+                      if (newValue == "") {
+                        setDuration(0);
+                      } else {
+                        const parsedValue = parseInt(newValue, 10);
+                        setDuration(parsedValue);
+                      }
+                    }}
+                    className="col-span-2 h-8 border-slate-600 focus:border-white"
+                  />
+                </div>
+              </div>
+              <div>
+                <Button
+                  type="submit"
+                  onClick={() => {
+                    if (!selectedCategory || duration <= 0) {
+                      toast.error("Please select a category and set a valid duration.");
+                      return;
+                    }
+                    setIsTimeVisible(true);
+                    startTimer();
+                    closePopover();
+                  }}
+                  className="grid float-right bg-white text-black hover:bg-slate-200 border border-slate-600"
+                >
+                  Start
+                </Button>
+              </div>
+            </div>
+          </PopoverContent>
+        </Popover>
+      </div>
+    </div>
   );
 };
 
